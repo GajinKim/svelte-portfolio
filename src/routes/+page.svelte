5982b633fb6d2ec88c4e3a01dd919151eb34ae5b
GaJin Kim <gajin2000@gmail.com>
2024-01-03 13:24:40 -0600

    add lighting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -35,6 +35,60 @@
         margin: 0;
         color: whitesmoke;
     }
+
+    
+.aws-subtitle {
+    font-size:  clamp(20px, 6vw, 40px);
+    line-height: clamp(20px, 6vw, 40px);
+    text-shadow: 0 0 5px #FF9900, 0 0 15px #FF9900, 0 0 20px #FF9900, 0 0 40px #FF9900, 0 0 60px #ff0000, 0 0 10px #ff8d00, 0 0 98px #ff0000;
+    color: #fff6a9;
+    text-align: center;
+    animation: blink 12s infinite;
+    -webkit-animation: blink 12s infinite;
+}
+
+@-webkit-keyframes blink {
+  20%,
+  24%,
+  55% {
+    color: #111;
+    text-shadow: none;
+  }
+
+  0%,
+  19%,
+  21%,
+  23%,
+  25%,
+  54%,
+  56%,
+  100% {
+
+  text-shadow: 0 0 5px #FF9900, 0 0 15px #FF9900, 0 0 20px #FF9900, 0 0 40px #FF9900, 0 0 60px #ff0000, 0 0 10px #ff8d00, 0 0 98px #ff0000;
+    color: #fff6a9;
+  }
+}
+
+@keyframes blink {
+  20%,
+  24%,
+  55% {
+    color: #111;
+    text-shadow: none;
+  }
+
+  0%,
+  19%,
+  21%,
+  23%,
+  25%,
+  54%,
+  56%,
+  100% {
+  text-shadow: 0 0 5px #FF9900, 0 0 15px #FF9900, 0 0 20px #FF9900, 0 0 40px #FF9900, 0 0 60px #ff0000, 0 0 10px #ff8d00, 0 0 98px #ff0000;
+    color: #fff6a9;
+  }
+}
 </style>
 
 <main>
@@ -42,5 +96,5 @@
         <Cat/>
     </div>
     <p class="name">Gajin Kim</p>
-    <p class="subtitle" ><span class="subtitle item" style="color:#FF9900;">AWS Cloud</span> Enthusiast</p>
+    <p class="subtitle" ><span class="aws-subtitle">AWS Cloud</span> Enthusiast</p>
 </main>
