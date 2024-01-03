42301595876e9a8dbaddf87a264503adc0576a26
GaJin Kim <gajin2000@gmail.com>
2024-01-02 18:59:15 -0600

    finished background for landing

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,31 @@
+<nav class="navbar">
+
+    <div  class="navbar-left">
+	    <a href="/">Home</a>
+    </div>
+
+    <div class="navbar-right">
+        <a href="/about">About</a>
+        <a href="/settings">Settings</a>
+        <a href="https://github.com/gajinkim">GitHub</a>
+    </div>
+
+</nav>
+
+<style>
+    .navbar {
+        display: flex;
+        justify-content: space-between;
+    }
+
+    .navbar-right {
+        gap: 15px;
+        display: flex;
+        justify-content: flex-end;
+    }
+
+</style>
+
+<body>
+    <slot></slot>
+</body>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,49 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Cat from '../components/Cat.svelte';
+</script>
+
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Alata">
+
+<style>
+    :global(body) {
+        background-color: black;
+    }
+
+    body {
+        font-family: "Alata", sans-serif;
+        text-align: center;
+        padding-top: 25vh;
+        padding-left: min(5px, 5vw);
+        padding-right: min(5px, 5vw);
+    }
+
+    .cat {
+        display: flex;
+        justify-content: center; /* Center horizontally */
+        align-items: center; /* Center vertically */
+    }
+
+    .name {
+        font-size: clamp(60px, 15vw, 120px);
+        margin: 0;
+        color: whitesmoke;
+
+    }
+
+    .subtitle {
+        font-size:  clamp(20px, 5vw, 40px);
+        margin: 0;
+        color: whitesmoke;
+    }
+
+</style>
+
+<body>
+    <div class="cat">
+        <Cat/>
+    </div>
+    <p class="name">GAJIN KIM</p>
+    <p class="subtitle">AWS Cloud Enthusiast</p>
+    <div id="colorful-streak"></div>
+
+</body>
