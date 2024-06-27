7ddaca54b7f371a9df533d948be4fd0077c845fd
Gajin Kim <gajin2000@gmail.com>
2024-06-27 11:58:30 -0500

    updated svelete porfolio

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -27,14 +27,11 @@
 
 <main>
 <h1>About Me</h1>
-<p>Back in 2012, I decided to try my hand at creating custom Tumblr themes and tumbled head first into the rabbit hole of coding and web development. Fast-forward to today, and I’ve had the privilege of building software for an advertising agency, a start-up, a student-led design studio, and a huge corporation.
     <br>
     <br>
 
-    My main focus these days is building products and leading projects for our clients at Upstatement. In my free time I've also released an online video course that covers everything you need to know to build a web app with the Spotify API.
     <br>
     <br>
-    When I’m not at the computer, I’m usually rock climbing, hanging out with my wife and two cats, or running around Hyrule searching for Korok seeds
     </p>
     <br>
 <h1>Skills</h1>
